c783b05396c7405c39cf2fb68589769427b1e4fb
M. Karthikeyan <113666490+Karthikeyan218@users.noreply.github.com>
2022-10-24 11:32:16 +0530

    Add files via upload

---
+++ Counter/counter.js
@@ -0,0 +1,25 @@
+const days=document.querySelector("#days")
+const hours=document.querySelector("#hours")
+const minutes=document.querySelector("#minutes")
+const seconds=document.querySelector("#seconds")
+
+
+function updateTimer(){
+const currentyear=new Date().getFullYear();
+const newyear=new Date(`january 1 ${currentyear+1} 00:00:00`)
+const currentdate=new Date()
+const diff=newyear-currentdate
+const d=Math.floor(diff/1000/60/60/24)
+
+const m=Math.floor((diff/1000/60/60)%24)
+
+const h=Math.floor((diff/1000/60)%60)
+
+const s=Math.floor((diff/1000)%60)
+days.innerHTML=d
+hours.innerHTML=h
+minutes.innerHTML=m
+seconds.innerHTML=s
+}
+updateTimer()
+setInterval(updateTimer,1000)
